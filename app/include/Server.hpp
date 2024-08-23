19f54ed2b2a7a1a26c0b000eac642ba1fd97650a
Nicolas Prado <nicolas.prado@lojasmm.com.br>
2024-08-23 17:32:35 -0300

    feat: Server.hpp implemented

--- app/include/Server.hpp
+++ app/include/Server.hpp
@@ -3,7 +3,6 @@
 
 #include <thread>
 #include <vector>
-#include <map>
 #include <string>
 #include <memory>
 
@@ -11,37 +10,24 @@
 #include "Socket.hpp"
 #include "Request.hpp"
 #include "Response.hpp"
-#include "HTTPParser.hpp"
-
-enum AvailableMethods {
-    GET,
-    POST,
-    PATCH,
-    DELETE
-};
 
 class Server {
 private:
     Socket* serverSocket;
     std::vector<std::unique_ptr<std::thread>> serverThreads;
-    const static HTTPParser httpParser;
 
-    std::map<Endpoint, Process> resources;
+    const HTTPController* httpController;
 
     void serveRequest(Socket* clientSocket);
-    void processRequest(Socket* clientSocket);
-    std::map<Endpoint, Process>::iterator findResource(Request);
-    std::vector<Endpoint> getResourceEndpoint(Endpoint targetEndpoint);
+
+    void sendResponse(Socket* clientSocket, const Response& res);
 public:
     Server();
-
-    void listen(unsigned int port);
     
-    void addResource(AvailableMethods method, std::string rawEndpoint, void (*handler)(Request, Response));
     void get(std::string rawEndpoint, void (*handler)(Request, Response));
     void post(std::string rawEndpoint, void (*handler)(Request, Response));
-    void patch(std::string rawEndpoint, void (*handler)(Request, Response));
-    void remove(std::string rawEndpoint, void (*handler)(Request, Response));
+
+    void listen(unsigned int port);
     
     // Test methods
     void promptResources(); 
